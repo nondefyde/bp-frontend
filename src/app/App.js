7f9b5dde9eb968e4939ef3c18ace1c3dfdc823e0
nondefyde <nondefyde@gmail.com>
2021-06-06 17:56:03 +0100

    added server script

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,7 @@
 // import "./App.scss";
 import {Router} from "@reach/router";
 import React from "react";
+import Filter from "../pages/Filter";
 import Personal from "../pages/Personal";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
         <div className={"App"}>
             <Router style={{height: '100%'}}>
                 <Personal path="/"/>
+                <Filter path="/filter"/>
             </Router>
         </div>
     );
