f304e41b95bd18b13a2679a7d7a91469a81251ef
nondefyde <nondefyde@gmail.com>
2021-06-06 18:13:09 +0100

    fixed

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-const express = require('express');
+const handler = require('serve-handler');
+const http = require('http');
 const path = require('path');
-const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+const server = http.createServer((request, response) => {
+  // You pass two more arguments for config and middleware
+  // More details here: https://github.com/zeit/serve-handler#options
+  console.log('Path: ', path.join(__dirname, 'build'));
+  
+  return handler(request, response, {
+    public: path.join(__dirname, 'build'),
+    cleanUrls: true,
+    rewrites: [
+      { source: '/**', destination: '/index.html' },
+    ],
+    directoryListing: false
+  })
 });
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT);
+const port = process.env.PORT || 3000;
+server.listen(port , () => {
+  console.log('Running at http://localhost:'+ port);
+});
